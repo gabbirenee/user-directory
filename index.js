b8dd6876c0fa322d279c07401ce39ef2ae0d3b49
Gabbi Forsythe <supergabbi18@gmail.com>
2018-05-15 11:00:55 -0400

    Focus user name after submit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const handleSubmit = function(ev){
     const age=form.age.value
     users.innerHTML += '<p>'+ userName +', '+ age + '</p>'
     form.reset()
+    form.userName.focus()
 }
 
 form.addEventListener('submit', handleSubmit)
