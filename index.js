f3bbfca25300166ca57358e82ea3fd486f0fb346
Gabbi Forsythe <supergabbi18@gmail.com>
2018-05-16 11:06:19 -0400

    Change a lot of how the code works. I forgot to commit periodically. Oops

--- index.js
+++ index.js
@@ -5,45 +5,48 @@ const form = document.querySelector('form#userForm')
 function renderColor(color) {
     const colorDiv = document.createElement('div')
     colorDiv.style.backgroundColor=color
-    colorDiv.style.width='6rem' //width=6rems
+    colorDiv.style.width='6rem'     //width=6rems
     colorDiv.style.height='3rem'    //height=3rems  rems are relative to the font size on the page
     return colorDiv
 }
 
-//Renders the Name, age, and favorite color list Items and appends them to the list
-function renderListItem (list, userName, age, favColor) {
-    const nameItem=document.createElement('li')
-    const ageItem=document.createElement('li')
-    const colorItem=document.createElement('li')
-
-    nameItem.textContent=`Name: ${userName}`
-    ageItem.textContent=`Age: ${age}`
-    colorItem.textContent='Favorite Color: '
-
-    colorItem.appendChild(renderColor(favColor)) //append colored div to the color item
-    list.appendChild(nameItem)  //appends name to list
-    list.appendChild(ageItem)   //appends age to list
-    list.appendChild(colorItem) //append color to the list
+//Renders the list items
+function renderListItem (label, value) {
+    const item = document.createElement('li')
+    item.textContent=`${label}: `
+    try {
+        item.appendChild(value)
+    } catch (e) {
+        item.textContent += value
+    }
+    return item
 }
 
 //Renders the list and appends the list to the empty html div
-function renderList (userName, age, favColor) {
+function renderList (user) {
     const list = document.createElement('ul')   //unordered list
 
-    renderListItem(list, userName, age, favColor)
+    Object.keys(user).map(function(label){  //Object.keys returns array of the keys
+        const item= renderListItem(label, user[label])
+        list.appendChild(item)
+    })
 
-    users.appendChild(list) //attatches the list to the div
+    return list
 }
 
 const handleSubmit = function(ev){
     ev.preventDefault()
     const form = ev.target
-    const userName=form.userName.value
-    const users=document.querySelector("#users")
-    const age=form.age.value
-    const favColor=form.favoriteColor.value
+    const user = {
+        'Name': form.userName.value,
+        'Age': form.age.value,
+        'Favorite Color': renderColor(form.favoriteColor.value),
+    }
     
-    renderList(userName, age, favColor);
+    const users=document.querySelector("#users")
+    
+    const list= renderList(user)
+    users.appendChild(list) //attatches the list to the div
    
     // users.innerHTML += `<p>${userName}, ${age}</p>`
     form.reset()
